8e4f4ee70ebfa903588a3bb932ee2ca35befb041
voimax <voimaxis@mail.ru>
2025-04-06 17:43:07 +0300

    Добавлено сохранение дерева в файл, новый класс для определения констант

--- src/main/java/ru/krista/yargu/epishin/web/WebServer.java
+++ src/main/java/ru/krista/yargu/epishin/web/WebServer.java
@@ -2,11 +2,19 @@ package ru.krista.yargu.epishin.web;
 
 import io.undertow.Undertow;
 import org.jboss.resteasy.plugins.server.undertow.UndertowJaxrsServer;
+import ru.krista.yargu.epishin.tree.Node;
+import ru.krista.yargu.epishin.web.utils.Constants;
+
+import java.io.IOException;
 
 /**
  * Консольное приложение, запускающее web-сервер.
  */
 public class WebServer {
+
+    private static RestApplication restApplication;
+    private static UndertowJaxrsServer server;
+
 
     public static void main(String[] argv) {
         start();
@@ -16,8 +24,26 @@ public class WebServer {
      * Запускает web-сервер. По окончании работы требуется ручная остановка процесса.
      */
     private static void start() {
-        UndertowJaxrsServer server = new UndertowJaxrsServer().start(Undertow.builder().addHttpListener(8081, "0.0.0.0"));
-        server.deploy(RestApplication.class);
-        System.out.println("Сервер запущен: http://localhost:8081/");
+        restApplication = new RestApplication();
+
+        server = new UndertowJaxrsServer()
+                .start(Undertow.builder()
+                        .addHttpListener(Constants.SERVER_PORT, Constants.SERVER_HOST));
+
+        server.deploy(restApplication);
+
+        System.out.println("Сервер запущен: "+Constants.SERVER_URL+Constants.TREE_BASE_PATH);
+
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+            System.out.println("Остановка сервера...");
+            Node tree = restApplication.getTree();
+            try {
+                tree.writeJsonFileTree(Constants.TREE_FILE_PATH);
+                server.stop();
+                System.out.println("Сервер остановлен успешно, дерево сохранено");
+            } catch (IOException e) {
+                System.err.println("Не удалось сохранить дерево: " + e.getMessage());
+            }
+        }));
     }
 }
---
+++ src/main/java/ru/krista/yargu/epishin/web/utils/Constants.java
@@ -0,0 +1,16 @@
+package ru.krista.yargu.epishin.web.utils;
+
+public final class Constants {
+    //WEBSERVER
+    public static final int SERVER_PORT = 8081;
+    public static final String SERVER_HOST = "0.0.0.0";
+    public static final String SERVER_URL = "http://localhost:"+SERVER_PORT;
+
+    //REST_APPLICATION
+    public static final String TREE_FILE_PATH = "ServerTree";
+
+    //TREE_PRESENTATION_CONTROLLER
+    public static final String TREE_BASE_PATH = "/tree";
+
+
+}
